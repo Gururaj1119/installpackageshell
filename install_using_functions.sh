ff469101c7cb42ec8ed114ed4fca7ebd5879a075
Gururaj1119 <114040066+Gururaj1119@users.noreply.github.com>
2022-12-23 21:56:42 +0530

    uploaded

---
+++ install_using_functions.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+install_aws_cli()
+{
+    sudo apt-get update
+    sudo apt-get install awscli
+    if [ $? -ne 0 ]; then
+       echo "error while installing awscli"
+    fi
+    echo "awscli installed successfully"
+}
+install_netcat()
+{
+    sudo apt-get update
+    sudo apt-get -y install netcat-traditional
+    if [ $? -ne 0 ]; then
+       echo "error while installing netcat"
+    fi
+    echo "netcat installed successfully"
+}
+install_aws_cli
+awscli --version
+install_netcat
+netcat --version
