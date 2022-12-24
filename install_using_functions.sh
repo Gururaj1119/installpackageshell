46078c6f2a760e427e2f839bcc5b850e6c4948f5
Gururaj1119 <gururaj1119@gmail.com>
2022-12-24 12:10:12 +0530

    added exit 1

--- install_using_functions.sh
+++ install_using_functions.sh
@@ -6,6 +6,7 @@ install_aws_cli()
     sudo apt-get install awscli
     if [ $? -ne 0 ]; then
        echo "error while installing awscli"
+       exit 1
     fi
     echo "awscli installed successfully"
 }
@@ -15,10 +16,12 @@ install_netcat()
     sudo apt-get -y install netcat-traditional
     if [ $? -ne 0 ]; then
        echo "error while installing netcat"
+       exit 1
     fi
     echo "netcat installed successfully"
 }
 install_aws_cli
 awscli --version
 install_netcat
-netcat --version
+#netcat --version
+nc -h
